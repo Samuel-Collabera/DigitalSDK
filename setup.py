eca4769941acea095b96c48b70c3843ba85c7d5e
Samuel-Collabera <143382241+Samuel-Collabera@users.noreply.github.com>
2024-05-17 10:48:20 +0530

    udpates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collabera_digital',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     install_requires=['streamlit==1.33.0'],
     python_requires='>=3.9'
